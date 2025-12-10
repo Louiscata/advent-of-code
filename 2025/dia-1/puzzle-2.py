aded02c4071b371b059801be68a5982f772c6763
Louiscata <lbcpdello@gmail.com>
2025-12-10 13:30:22 +0100

    Día 1 puzle 2

---
+++ 2025/dia-1/puzzle-2.py
@@ -0,0 +1,21 @@
+file = open("puzzle-1-input.txt", "r")
+
+pos = 50
+pwd = 0
+
+for line in file:
+    dir = line[0]
+    dist = int(line[1:])
+    match dir:
+        case 'R':
+            pos += dist
+            pwd += pos // 100
+        case 'L':
+            first = pos if pos != 0 else 100
+            if dist >= first:
+                pwd += (dist - first) // 100 + 1
+            pos -= dist
+                
+    pos %= 100
+            
+print(pwd)
