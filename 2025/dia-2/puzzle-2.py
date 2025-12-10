0fc98be8b25207ee7a4b273a0f78cc481f74a7de
Louiscata <lbcpdello@gmail.com>
2025-12-10 16:46:37 +0100

    Cambio menor en día 2 puzle 2

--- 2025/dia-2/puzzle-2.py
+++ 2025/dia-2/puzzle-2.py
@@ -14,7 +14,6 @@ for interval in intervals:
         digits = len(id_str)
         for pattern_length in range(1, digits // 2 + 1):
             if digits % pattern_length == 0:
-                check = 0
                 flag = 1
                 for i in range(0, digits - pattern_length, pattern_length):
                     i_p1 = i + pattern_length
